1e13a941810ac4dc9a3678c3f100cfd52db3ff1d
rodrigorgs <rodrigorgs@gmail.com>
2014-07-19 20:05:28 -0300

    Classify architectural violations.

--- script/parse-violmessage.rb
+++ script/parse-violmessage.rb
@@ -16,6 +16,20 @@ ENVIRONMENT = /[^ ]+/
 VERSION = /[^ ]+/
 BUNDLE = /[^ ]+/
 
+# violation types:
+# - extends/implements
+# - instantiates
+# - wrong parameter
+# - references
+# - API vs non-API
+
+# http://stackoverflow.com/questions/2587076/eclipse-warning-methodname-has-non-api-return-type-parameterizedtype
+
+## Violation types:
+violtypes = %w(
+inheritance instantiate api api inheritance environment environment reference
+inheritance api api api api inheritance inheritance --- reference inheritance
+--- environment --- --- inheritance)
 
 templates = [
   /^(?<source>#{TYPE}) illegally extends (?<target>#{TYPE})$/,
@@ -44,9 +58,14 @@ templates = [
 
 def match_one(text, regexes)
   matches = []
+  index = 0
   regexes.each do |regex|
     m = regex.match(text)
-    matches << m unless m.nil?
+    if !m.nil?
+      matches << m
+      yield index
+    end
+    index += 1
   end
   raise RuntimeError, "A string matches two regexes: '#{text}'" if matches.size > 1
   return matches.empty? ? nil : matches[0]
@@ -63,7 +82,10 @@ if __FILE__ == $0
     # Replace strange numbers that appear on some signatures, e.g. SWT_AWT_.11.
     line.gsub!(/\.\d+\./, '.method')
 
-    m = match_one(line, templates)
+    violtype = nil
+    m = match_one(line, templates) do |index|
+      violtype = violtypes[index]
+    end
     raise RuntimeError, "No match for #{line}" if m.nil?
 
     # remove internal class names (after $)
@@ -72,7 +94,7 @@ if __FILE__ == $0
     target = m.names.include?('target') ? m['target'] : nil
     target = target.gsub(/\$[^.]+/, '') if target
     
-    results << [i, line, source, target]
+    results << [i, line, violtype, source, target]
     klasses << source unless source.nil?
     klasses << target unless target.nil?
   end
@@ -80,7 +102,7 @@ if __FILE__ == $0
   puts "#{i} line(s) processed."
 
   File.open('../data/viol-klasses.tsv', 'w') do |f|
-    f.puts "violation\tdescription\tsource\ttarget"
+    f.puts "violation\tdescription\tvioltype\tsource\ttarget"
     f.puts results.map { |l| l.join("\t") }.join("\n")
   end
   File.open('../data/klasses.txt', 'w') { |f| f.puts klasses.sort.uniq.join("\n") }
