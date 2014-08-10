947c04f08cf6bdc9acbdd6518be04b5126dc2316
rodrigorgs <rodrigorgs@gmail.com>
2014-08-10 16:24:20 -0300

    Collect creation date/time for extra bugs.

--- script/parse-extra-bugs.R
+++ script/parse-extra-bugs.R
@@ -14,21 +14,33 @@
 #
 # $ grep -l '<td>REOPENED' history-* | sort | uniq | sed -e 's/history-//' | paste -sd ' '
 #
+##########
+#
+# extra-bugs-creation.grep generated from command
+#
+# $ grep "<creation_ts>.*</creation_ts>" bug-* > extra-bugs-creation.grep
+#
 
 library(stringr)
+library(dplyr)
 
 # Parse extra bugs
 
 reopened.bugs <- scan("../raw-data/extra-bugs-reopened.grep")
-lines <- readLines("../raw-data/extra-bugs-versions.grep")
-m <- str_match(lines, "^bug-(\\d+):\\s+<version>(.*?)</version>")[, c(2, 3)]
+grep.version <- readLines("../raw-data/extra-bugs-versions.grep")
+grep.creation <- readLines("../raw-data/extra-bugs-creation.grep")
 
-extra.bugs <- data.frame(bug = as.integer(m[, 1]), 
-	initial.time = NA,
+versions <- str_match(grep.version, "^bug-(\\d+):\\s+<version>(.*?)</version>")[, c(2, 3)]
+creations <- str_match(grep.creation, "^bug-(\\d+):\\s+<creation_ts>(.*?)</creation_ts>")[, c(2, 3)]
+
+stopifnot(all(versions[, 1] == creations[, 1]))
+
+extra.bugs <- data.frame(bug = as.integer(versions[, 1]), 
+	initial.time = as.POSIXct(creations[, 2]),
 	final.time = NA,
 	status = NA,
 	resolution = NA,
-	version = m[, 2],
+	version = versions[, 2],
 	description = NA,
 	reopened = NA,
 	stringsAsFactors=F)
