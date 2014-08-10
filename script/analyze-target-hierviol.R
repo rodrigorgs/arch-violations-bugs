b330a28bc09a6aa2a33b068136b3d49763d48c50
rodrigorgs <rodrigorgs@gmail.com>
2014-08-10 16:27:15 -0300

    Add script to list bugs that were fixed by modifying a class with an hierarchical violation.

---
+++ script/analyze-target-hierviol.R
@@ -0,0 +1,52 @@
+rm(list=ls())
+library(dplyr)
+
+#' Which bugs/klasses are target of hierarchy violations? which of these were reopened, which were not?
+
+commits <- readRDS("../data/commits-with-releases.rds")
+changed.klasses <- readRDS("../data/changed-klasses.rds")
+bugs <- readRDS("../data/bugs-extended.rds")
+# bugs <- readRDS("../data/bugs.rds")
+# klassloc <- readRDS("../data/klassloc.rds")
+# releases <- readRDS("../data/eclipse-releases.rds")
+# violations <- readRDS("../data/violations.rds")
+# viol.releases <- readRDS("../data/viol-releases.rds")
+
+commits$bug <- as.numeric(commits$bug)
+bugs$bug <- as.numeric(bugs$bug)
+
+# violations <- subset(violations, violtype != "---")
+
+metrics <- readRDS("../data/metrics.rds")
+
+targhier <- subset(metrics, endpoint == "target" & violtype == "hierarchy" & violations > 0)
+
+######################
+
+# reopened <- subset(targhier, reopened)
+
+bugs.kr <- commits %.%
+	inner_join(changed.klasses, by="commit") %.%
+	inner_join(bugs) %.%
+	group_by(klass, release, bug, description) %.%
+	summarise(reopened = any(reopened)) %.%
+	arrange(klass, release) #%.%
+	# select(klass, release, bug, reopened)
+
+x <- bugs.kr %.%
+	inner_join(targhier, by=c("klass", "release", "reopened")) %.%
+	arrange(reopened, bug) %.%
+	select(klass, release, reopened, bug, description)
+
+# nrow(x)
+# print.data.frame(x[, c("klass", "bug", "reopened")])
+
+
+clip <- pipe("pbcopy", "w")
+write.table(x, file=clip, sep="\t")
+close(clip)
+# View(x)
+
+#' # PROBLEM:
+
+#' Maybe the result is biased by the fact that there's one single bug with multiple violation classes (bug 395213)
