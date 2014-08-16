97d002b092bec53a7e275c7691517af55a4077e6
rodrigorgs <rodrigorgs@gmail.com>
2014-08-16 13:56:52 -0300

    Remove extraneous output message.

--- script/find-bug-inducing-commits.rb
+++ script/find-bug-inducing-commits.rb
@@ -109,8 +109,8 @@ if __FILE__ == $0
       # -c: Use the same output mode as git-annotate
 
       out = output_from_command(%Q[git blame --date=iso8601 -clwMC #{rev}^ -- "#{change.file}"])
-      puts "from #{gitrepo_path}"
-      puts out
+      STDERR.puts "from #{gitrepo_path}"
+      STDERR.puts out
       if (out.empty?)
         STDERR.puts "No output for commit #{rev} (blame). Skipping..."
         next
